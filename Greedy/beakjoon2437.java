3c00ff49485c4458a1e882b9bd790f12d7203d38
dsKU <54886969+dsKU@users.noreply.github.com>
2022-01-17 22:45:54 +0900

    Create beakjoon2437.java

---
+++ Greedy/beakjoon2437.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class beakjoon2437 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] val =new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N; i++){
+            val[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(val);
+            
+        int sum = 0;
+        for(int i = 0 ; i < N; i++){
+            if(sum + 1 >= val[i]){
+                sum+=val[i];
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println(sum+1);
+    }
+}
